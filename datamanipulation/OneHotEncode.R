7be89f297464ca8d54ce3748a13bbd6c2585ef76
Dhruv <42606683+dA505819@users.noreply.github.com>
2019-11-18 20:53:12 +0530

    Renamed OneHotEncode.R
    Corrected Typo from "Code" to "Encode".

--- datamanipulation/OneHotEncode.R
+++ datamanipulation/OneHotEncode.R
@@ -4,7 +4,7 @@ ind<-sample(2,nrow(diamonds),replace = T,prob = c(0.7,0.3))
 train.set<-diamonds[ind==1,]
 test.set<-diamonds[ind==2,]
 
-# one hot code using model.matrix(built-in function)
+# one hot encode using model.matrix(built-in function)
 #along with %>%, select_if, bind_cols, as.tibble in dplyr and tibble
 train<-bind_cols(select_if(train.set,is.numeric),model.matrix(~cut-1,train.set) %>% as.tibble(),
                 model.matrix(~color-1,train.set) %>% as.tibble(),model.matrix(~clarity-1,train.set) %>% as.tibble())
