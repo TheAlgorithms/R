5ecd159a1dc71799d771465ef6ac554359edebc8
Stepfen Shawn <m18824909883@163.com>
2020-07-23 16:49:49 +0800

    Remove the Chinese comment.

--- AssociationAlgorithm/Apriori.R
+++ AssociationAlgorithm/Apriori.R
@@ -3,7 +3,6 @@ groceries<-read.transactions("groceries.csv",sep = ",")
 summary(groceries)
 itemFrequencyPlot(groceries,topN=20)
 
-#使用image可视化稀疏矩阵，sample随机抽样
 #sample for randomly extracting samples, image function for visualing sparse matrix
 image(sample(groceries,100))
 groceries_rule<-apriori(data = groceries,parameter =list(support=0.006,confidence=0.25,minlen=2))
