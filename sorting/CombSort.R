523c716d4e120e485ffc5bcb479ffd8ec041b28c
Mertcan Davulcu <54983926+mertcandav@users.noreply.github.com>
2020-10-04 09:02:03 +0300

    Add comb sort algorithm

---
+++ sorting/CombSort.R
@@ -0,0 +1,26 @@
+# Comb sort in R:
+
+comb.sort <- function(a) {
+  gap <- length(a)
+  swaps <- 1
+  while (gap > 1 && swaps == 1) {
+    gap = floor(gap / 1.3)
+    if (gap < 1) {
+      gap = 1
+    }
+    swaps = 0
+    i = 1
+    while (i + gap <= length(a)) {
+      if (a[i] > a[i + gap]) {
+        a[c(i, i + gap)] <- a[c(i + gap, i)]
+        swaps = 1
+      }
+      i <- i + 1
+    }
+  }  
+  return(a) 
+}
+
+# Example:
+# comb.sort(sample(1:100,10))
+# [1] 9 49 50 51 56 60 61 71 86 95
