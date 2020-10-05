95eaf3eb269c69a8fe3b35cd64dc26c789125c95
Mertcan Davulcu <54983926+mertcandav@users.noreply.github.com>
2020-10-05 16:38:49 +0300

    Update Gnome sort.R

--- sorting/Gnome sort.R
+++ sorting/Gnome sort.R
@@ -1,25 +1,26 @@
 # Gnome sort in R:
 
+# Appy gnome sort algorithm
 gnome.sort <- function(elements.vec) {
- index <- 1
- j <- 1
- while(index < length(elements.vec)) {
-   if(elements.vec[index] <= elements.vec[index + 1]) {
-     index <- j
-     j <- j + 1
+ index <- 1 # Create index variable and set value to 1
+ j <- 1 # Create j variable and set value to 1
+ while(index < length(elements.vec)) { # Return while loop if index lower then element count
+   if(elements.vec[index] <= elements.vec[index + 1]) { # If current item lower or equals to next item
+     index <- j # Set index to value of j
+     j <- j + 1 # Increase j by one
     }
-    else {
-      temp <- elements.vec[index]
-      elements.vec[index] <- elements.vec[index + 1]
-      elements.vec[index + 1] <- temp
-      index <- index - 1
-      if(index == 0) {
-        index <- j
-        j <- j + 1
+    else { # If not
+      temp <- elements.vec[index] # Get current element
+      elements.vec[index] <- elements.vec[index + 1] # Set current element to next element
+      elements.vec[index + 1] <- temp # Set next element to current element
+      index <- index - 1 # Decrease j by one
+      if(index == 0) { # If index is first
+        index <- j # Set index to value of j
+        j <- j + 1 # Increase j by one
       } 
     }
   }
-  return(elements.vec)
+  return(elements.vec) # Return elements
 }
 
 # Example:
