a7d01ff994fa3735bcfdc8341946c2e1cb2eb077
Mertcan Davulcu <54983926+mertcandav@users.noreply.github.com>
2021-03-27 09:05:17 +0300

    Delete Logistic Regression.R

--- classification/Logistic Regression.R
+++
@@ -1,6 +0,0 @@
-x <- cbind(x_train,y_train)
-# Train the model using the training sets and check score
-logistic <- glm(y_train ~ ., data = x,family='binomial')
-summary(logistic)
-# Predict Output
-predicted= predict(logistic,x_test)
