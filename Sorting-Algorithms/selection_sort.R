1162b5f40d50f14fe4b27fb640bb9f07986bd0d1
Mertcan Davulcu <54983926+mertcandav@users.noreply.github.com>
2021-06-19 08:45:29 +0300

    selection_sort.R: Avoid algorithm repetition.

--- Sorting-Algorithms/selection_sort.R
+++ Sorting-Algorithms/selection_sort.R
@@ -2,34 +2,18 @@
 
 selection.sort <- function(elements.vec, ascending = TRUE) {
   max <- length(elements.vec)
-  if (ascending) {
-    for (j in 1:(max - 1)) {
-      m <- elements.vec[j]
-      p <- j
-      for(k in (j + 1):max) {
-        if(elements.vec[k] < m) {
-          m <- elements.vec[k]
-          p <- k
-        } 
-      } 
-        elements.vec[p] <- elements.vec[j]
-        elements.vec[j] <- m
-      } 
+  for (j in 1:(max - 1)) {
+    m <- elements.vec[j]
+    p <- j
+    for(k in (j + 1):max) {
+      if(ascending && elements.vec[k] < m || !ascending && elements.vec[k] > m) {
+        m <- elements.vec[k]
+        p <- k
+      }
     } 
-    else {
-      for (j in 1:(max - 1)) {
-        m <- elements.vec[j]
-        p <- j
-        for(k in (j + 1):max) {
-          if(elements.vec[k] > m) {
-            m <- elements.vec[k]
-            p <- k
-          } 
-        } 
-        elements.vec[p] <- elements.vec[j]
-        elements.vec[j] <- m
-      } 
-    }
+    elements.vec[p] <- elements.vec[j]
+    elements.vec[j] <- m
+  } 
   return(elements.vec)
 }
 
