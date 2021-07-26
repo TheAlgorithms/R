43c68327be8d3a276bd6b352920994a69dca94b6
Alex Garland <alex@numinar.com>
2021-07-25 18:18:19 -0700

    Modified Raw Linear Regression to use SVD, which is more numerically stable and popular practice these days

--- Regression-Algorithms/linearRegressionRawR.R
+++ Regression-Algorithms/linearRegressionRawR.R
@@ -1,7 +1,8 @@
 ols<-function(y,x){
     x<-as.matrix(x)
     x<-cbind(intercept=1,x)
-    return(solve(t(x) %*% x) %*% t(x) %*% y)
+    decomp <- svd(x)
+    return(decomp$v %*% diag(1 / decomp$d) %*% t(decomp$u) %*% y)
   }
 
   ols(y=diamonds$price,x=diamonds %>% select(-price)) %>% print()
