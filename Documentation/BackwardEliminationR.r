04f05ac9bbdbfc3cf89837d1180d0990ff640c76
Kaustubh Singh <kaustbusiness@gmail.com>
2022-10-11 16:18:18 +0530

    Create BackwardEliminationR.r
    Added Code to implement backward elimination in R, in the Multiple Linear Regression Section of Mathematics

---
+++ Documentation/BackwardEliminationR.r
@@ -0,0 +1,19 @@
+# How to do automatic Backward Elimination in R
+
+backwardElimination <- function(x, sl) {
+    numVars = length(x)
+    for (i in c(1:numVars)){
+      regressor = lm(formula = Profit ~ ., data = x)
+      maxVar = max(coef(summary(regressor))[c(2:numVars), "Pr(>|t|)"])
+      if (maxVar > sl){
+        j = which(coef(summary(regressor))[c(2:numVars), "Pr(>|t|)"] == maxVar)
+        x = x[, -j]
+      }
+      numVars = numVars - 1
+    }
+    return(summary(regressor))
+  }
+  
+  SL = 0.05
+  dataset = dataset[, c(1,2,3,4,5)]
+  backwardElimination(training_set, SL)
