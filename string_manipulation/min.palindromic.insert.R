4ac29d9118dc372bc690d21538cf4f1a4182fcce
Arpita Roy <100989922+Arpita23r@users.noreply.github.com>
2025-10-11 14:49:30 +0530

    Update min.palindromic.insert.R

--- string_manipulation/min.palindromic.insert.R
+++ string_manipulation/min.palindromic.insert.R
@@ -1,3 +1,16 @@
+#' Computes the minimum number of characters to insert to make a string a palindrome.
+#'
+#' @param s A character string (case-insensitive).
+#' @return A list containing:
+#'   \item{min_insertions}{Minimum number of insertions required.}
+#'   \item{palindrome}{One possible resulting palindrome.}
+#' @details
+#' Uses dynamic programming to compute the minimum insertions in O(n^2) time and space.
+#' Also reconstructs one possible palindrome.
+#'
+#' @examples
+#' min_palindrome_insertion("race")
+#' # Returns 3 insertions and palindrome "ecarace"
 # Minimum Palindrome Insertion in R
 # Computes the minimum number of characters to insert to make a string a palindrome
 # Also prints one possible resulting palindrome.
@@ -24,7 +37,8 @@ min_palindrome_insertion <- function(s) {
   # Reconstruct one possible palindrome
   i <- 1
   j <- n
-  res <- character(length_sub <- dp[1, n] + n)
+  length_sub <- dp[1, n] + n
+  res <- character(length_sub)
   left <- 1
   right <- length(res)
   
@@ -34,29 +48,25 @@ min_palindrome_insertion <- function(s) {
       res[right] <- chars[j]
       i <- i + 1
       j <- j - 1
-      left <- left + 1
-      right <- right - 1
     } else if (dp[i + 1, j] < dp[i, j - 1]) {
       res[left] <- chars[i]
       res[right] <- chars[i]
       i <- i + 1
-      left <- left + 1
-      right <- right - 1
     } else {
       res[left] <- chars[j]
       res[right] <- chars[j]
       j <- j - 1
-      left <- left + 1
-      right <- right - 1
     }
+    left <- left + 1
+    right <- right - 1
   }
   
   list(min_insertions = dp[1, n], palindrome = paste(res, collapse = ""))
 }
 
-# Interactive input
+# --- Interactive input ---
 s <- tolower(readline("Enter a string: "))
 
 result <- min_palindrome_insertion(s)
 cat("Minimum insertions required:", result$min_insertions, "\n")
-cat("One possible resulting palindrome:", result$palindrome, "\n")
+cat("One possible resulting palindrome:", result$palindrome, "\n")
